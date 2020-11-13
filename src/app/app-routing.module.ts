5a6c339fe8af3d0f2e62c7969020740a4db2f356
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2020-11-12 20:29:09 -0600

    Template ngModel y ngSubmit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { ReactiveComponent } from './pages/reactive/reactive.component';
 const routes: Routes = [
   { path:'template', component: TemplateComponent },
   { path:'reactivo', component: ReactiveComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'reactivo' }
+  { path: '**', pathMatch: 'full', redirectTo: 'template' }
 ];
 
 @NgModule({
